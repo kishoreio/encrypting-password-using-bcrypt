df9cde6d71c5057aa3e40ae9cd9eb62081f1c5a4
Kishore S <kishore@Kishores-MacBook-Pro.local>
2020-02-26 20:10:48 +0530

    added brcypt for encrypting password

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+//function to hash the plain password
+const generateHash = (plainPassword) => {
+  return new Promise(function(resolve, reject) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) reject(err);
+      bcrypt.hash(plainPassword, salt, function(err, hashPassword) {
+        err ? reject(err) : resolve(hashPassword);
+      });
+    });
+  });
+};
+
+//function to compare the plainPassword and hashedPassword
+const compareHash = (plainPassword, hashPassword) => {
+  return new Promise(function(resolve, reject) {
+    bcrypt.compare(plainPassword, hashPassword, function(err, hashResult) {
+      err ? reject(err) : resolve(hashResult);
+    });
+  });
+};
+
+const plainPassword = "admin123";
+generateHash(plainPassword)
+  .then((result) => {
+    compareHash(plainPassword, result)
+      .then((newResult) => {
+        console.log(newResult);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
